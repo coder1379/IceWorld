a957cb5c6899cc8061793cfee4645ef1dd6c3ea2
majie <majie@wy.com>
2020-08-11 21:16:53 +0800

    1

--- environments/befprod/backend/config/main-local.php
+++ environments/befprod/backend/config/main-local.php
@@ -1,14 +1,6 @@
 <?php
 return [
     'components' => [
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=name_db;port=3306',
-            'tablePrefix' => 'm_',
-            'username' => 'root',
-            'password' => 'password',
-            'charset' => 'utf8',
-        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
