a26ccbd0cb6828f5b3baa03aad228714fff9600f
majie <majie@wy.com>
2020-09-01 14:24:22 +0800

    1

--- backend/giitemplate/crud/default/controller.php
+++ backend/giitemplate/crud/default/controller.php
@@ -136,12 +136,12 @@ class <?= $controllerClass ?> extends <?= StringHelper::basename($generator->bas
         if ( $model->load(Yii::$app->request->post())==true) {
             //添加添加时间和添加的管理员代码
             $allAttributeLabels = $model->attributeLabels();
-            if(!empty($allAttributeLabels['add_time']) && (empty($model->add_time) || $model->add_time == '0000-00-00 00:00:00' )){
-                $model->add_time = date('Y-m-d H:i:s',time());
+            if(!empty($allAttributeLabels['add_time']) && empty($model->add_time)){
+                $model->add_time = time();
             }
-
-            if(!empty($allAttributeLabels['update_time']) && (empty($model->update_time) || $model->update_time == '0000-00-00 00:00:00' )){
-                $model->update_time = date('Y-m-d H:i:s',time());
+            
+            if(!empty($allAttributeLabels['update_time']) && empty($model->update_time)){
+                $model->update_time = time();
             }
 
             if(!empty($allAttributeLabels['add_admin_id']) && empty($model->add_admin_id)){
