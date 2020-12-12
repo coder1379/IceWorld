96217994f566baebe470b23e889e2b41d91b0c46
majie <majie@wy.com>
2020-12-12 21:51:46 +0800

    1

--- common/services/log/AccessBaseLogic.php
+++ common/services/log/AccessBaseLogic.php
@@ -12,19 +12,10 @@ use common\ComBase;
  */
 class AccessBaseLogic
 {
-    public function save($request,$file)
+    public function save($route,$request,$file)
     {
-        try {
             //  $this->route; 获取路径如：feedback/message/create
 
-            $startTime = intval(YII_BEGIN_TIME * 10000);
-            $endTime = intval(microtime(true) * 10000);
-            $useTime = ceil(($endTime - $startTime) / 10);
-            echo $useTime;
-
-        } catch (\Exception $exc) {
-            Yii::error('访问日志写入错误:' . $exc->getMessage());
-        }
 
     }
 
