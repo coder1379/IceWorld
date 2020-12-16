4d124cc3cf94c3f17a926502ee4b4f1e62a87105
majie <majie@wy.com>
2020-12-16 21:59:17 +0800

    1

--- common/base/UserCommon.php
+++ common/base/UserCommon.php
@@ -53,10 +53,11 @@ class UserCommon
      * 获取用户登录token
      * @param $userId
      * @param null $userType
+     * @param int $appId 应用id 默认0
      * @return array
      * @throws \Exception
      */
-    public static function getUserLoginToken($userId,$userType=null){
+    public static function getUserLoginToken($userId,$userType=null,$appId = 0){
 
         if(empty($userId) || $userType==null){
             throw new \Exception('user_id或type不能为空');
@@ -72,6 +73,7 @@ class UserCommon
             'u_t' => $userType,//user_type
             'o_t'=>$outTime,//过期时间
             'r_s' => StringHandle::getRandomString(4),//写入4位随机码生成不同token
+            'a_i' => $appId, //写入应用id到jwt,根据业务判断使用
         ];
 
         $jwtStr = JWT::encode($mainArr, Yii::$app->params['jwt_md5_key']);
@@ -110,15 +112,16 @@ class UserCommon
     /**
      * 获取用户登录绑定信息通过需要密码登录的类型   ******注意这里获取的是密码登录表*****
      * @param $key
+     * @param int $appId 应用id 默认0
      * @return \yii\db\DataReader
      * @throws \yii\db\Exception
      */
-    public static function getUserLoginBindWithPwdTypes($key)
+    public static function getUserLoginBindWithPwdTypes($key,$appId=0)
     {
         //注意返回类型都为字符串问题
         $allSearchType = [self::USER_LOGIN_TYPE_USERNAME, self::USER_LOGIN_TYPE_MOBILE, self::USER_LOGIN_TYPE_EMAIL];//用户名,手机号,邮箱归为密码登录3个不能重复，所以查询为3个一起查询
-        $sql = 'select id,user_id,type,bind_key from {{%user_login_bind}} where type in ('.implode(",",$allSearchType).') and bind_key=:bind_key limit 1';
-        return Yii::$app->db->createCommand($sql, [':bind_key' => $key])->queryOne();
+        $sql = 'select id,user_id,type,bind_key from {{%user_login_bind}} where type in ('.implode(",",$allSearchType).') and bind_key=:bind_key and app_id=:app_id limit 1';
+        return Yii::$app->db->createCommand($sql, [':bind_key' => $key,':app_id'=>$appId])->queryOne();
     }
 
     /**
@@ -134,14 +137,14 @@ class UserCommon
     }
 
     /**
-     * 获取用户登录设备通过user_id + 短token
+     * 获取用户登录设备通过user_id + 短token + app_id
      * @param $userId
      * @param $shortToken
      * @return array|false
      * @throws \yii\db\Exception
      */
-    public static function getUserDeviceByUserIdToken($userId,$shortToken){
-       return Yii::$app->db->createCommand('select id,user_id,device_code,type,token from {{%user_login_device}} where user_id=:user_id and token=:token limit 1 ', [':user_id' => $userId, ':token' => $shortToken])->queryOne();
+    public static function getUserDeviceByUserIdToken($userId,$shortToken,$appId = 0){
+       return Yii::$app->db->createCommand('select id,user_id,device_code,type,token from {{%user_login_device}} where user_id=:user_id and token=:token and app_id=:app_id limit 1 ', [':user_id' => $userId, ':token' => $shortToken,':app_id' => $appId])->queryOne();
     }
 
 }
--- common/controllers/ApiCommonAuthContoller.php
+++ common/controllers/ApiCommonAuthContoller.php
@@ -1,47 +1,50 @@
 <?php
+
 namespace common\controllers;
 
 use common\base\UserCommon;
 use common\ComBase;
 use Yii;
 use yii\helpers\Json;
+
 /**
  * 接口需用户登录及权限控制器基类
  */
-
 class ApiCommonAuthContoller extends ApiCommonContoller
 {
     public $allowAccessActions = null;//设置不校验用户允许访问的actions
     public $verifyShortTokenActions = null;//需要进行短token校验的action,注意大小写要保持一致，是区分大小写的
+
     //public $loginAccessActions = [];//设置登录就能访问的页面
     public function beforeAction($action)
     {
 
         $verifyCode = $this->setUser();
-        if(Yii::$app->params['open_jwt_expire_verify']){
-            if($verifyCode == ComBase::CODE_LOGIN_EXPIRE){
+        if (Yii::$app->params['open_jwt_expire_verify']) {
+            if ($verifyCode == ComBase::CODE_LOGIN_EXPIRE) {
                 //jwt过期，返回要求前端续签
-                echo Json::encode($this->getJsonArray([],ComBase::CODE_LOGIN_EXPIRE,ComBase::MESSAGE_LOGIN_EXPIRE));
+                echo Json::encode($this->getJsonArray([], ComBase::CODE_LOGIN_EXPIRE, ComBase::MESSAGE_LOGIN_EXPIRE));
                 exit();
             }
         }
-        $actionId=$action->id;
+        $actionId = $action->id;
 
         //先校验是否在允许例外的actions数组内
-        if(empty($this->allowAccessActions) || !in_array($actionId,$this->allowAccessActions,true)){
-            if(empty($this->userId) || empty($this->shortToken) || $this->userType<=0){ //userId,userType<=0,shortToken为空均表示未登录
+        if (empty($this->allowAccessActions) || !in_array($actionId, $this->allowAccessActions, true)) {
+            if (empty($this->userId) || empty($this->shortToken) || $this->userType <= 0) { //userId,userType<=0,shortToken为空均表示未登录
                 //没有权限
-                echo Json::encode($this->getJsonArray([],ComBase::CODE_NO_LOGIN_ERROR,ComBase::MESSAGE_NO_LOGIN_ERROR));
+                echo Json::encode($this->getJsonArray([], ComBase::CODE_NO_LOGIN_ERROR, ComBase::MESSAGE_NO_LOGIN_ERROR));
                 exit();
             }
         }
 
         //二次校验是否在需要进行短token数据库查询验证的actions数组内，存在则进行token数据库校验
-        if(!empty($this->verifyShortTokenActions) && in_array($actionId,$this->verifyShortTokenActions)){
-            $deviceData = UserCommon::getUserDeviceByUserIdToken($this->userId, $this->shortToken);
-            if(empty($deviceData)){
+        if (!empty($this->verifyShortTokenActions) && in_array($actionId, $this->verifyShortTokenActions)) {
+            $appId = 0;//此处app_id使用默认0 根据业务调整是否需要验证app_id，例如是否运行跨app使用相同jwt等
+            $deviceData = UserCommon::getUserDeviceByUserIdToken($this->userId, $this->shortToken, $appId);
+            if (empty($deviceData)) {
                 //通过token未查询到对应设备，没有权限
-                echo Json::encode($this->getJsonArray([],ComBase::CODE_NO_LOGIN_ERROR,ComBase::MESSAGE_NO_LOGIN_ERROR));
+                echo Json::encode($this->getJsonArray([], ComBase::CODE_NO_LOGIN_ERROR, ComBase::MESSAGE_NO_LOGIN_ERROR));
                 exit();
             }
 
