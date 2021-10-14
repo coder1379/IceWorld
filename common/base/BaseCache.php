606d4294df3392daae89642d97cdb38f27dba40b
coder1379 <majiegame@163.com>
2021-10-14 19:57:10 +0800

    1

--- common/base/BaseCache.php
+++ common/base/BaseCache.php
@@ -11,6 +11,8 @@ use Yii;
 class BaseCache
 {
 
+    private static $cacheType = 1;// 1=文件缓存，2=redis 缓存类型 根据业务需要修改
+
     /**
      * 同步设置缓存值与过期时间
      * @param $key
@@ -24,20 +26,19 @@ class BaseCache
         $expireTime = intval($expireTime);
         $key = strval($key);
 
-        //使用redis 与cache 2选一 start
-        /*if (empty($db)) {
-            $db = Yii::$app->redis;
+        if(self::$cacheType===2){
+            // redis
+            if (empty($db)) {
+                $db = Yii::$app->redis;
+            }
+            return $db->setex($key, $expireTime, $value);
+        }else if(self::$cacheType===1){
+            // 文件
+            if (empty($db)) {
+                $db = Yii::$app->cache;
+            }
+            return $db->set($key, $value, $expireTime);
         }
-        return $db->setex($key, $expireTime, $value);*/
-        //使用redis 与cache 2选一 end
-
-
-        //使用文件cache保存 start 与上注释redis 2选一 默认使用的文件
-        if (empty($db)) {
-            $db = Yii::$app->cache;
-        }
-        return $db->set($key, $value, $expireTime);
-        //使用文件cache end
     }
 
     /**
@@ -48,22 +49,21 @@ class BaseCache
      */
     public static function getVal($key, $db = null)
     {
-        $key = strval($key);//设置项目id前缀避免重复覆盖
+        $key = strval($key);
 
-        //使用redis 与cache 2选一 start
-        /*if (empty($db)) {
-            $db = Yii::$app->redis;
+        if(self::$cacheType===2){
+            //redis
+            if (empty($db)) {
+                $db = Yii::$app->redis;
+            }
+            return $db->get($key);
+        }else if(self::$cacheType===1){
+            //file
+            if (empty($db)) {
+                $db = Yii::$app->cache;
+            }
+            return $db->get($key);
         }
-        return $db->get($key);*/
-        //使用redis 与cache 2选一 end
-
-
-        //使用文件cache start 与上注释redis 2选一 默认使用的文件
-        if (empty($db)) {
-            $db = Yii::$app->cache;
-        }
-        return $db->get($key);
-        //使用文件cache end
     }
 
     /**
@@ -74,21 +74,64 @@ class BaseCache
      */
     public static function deleteVal($key, $db = null)
     {
-        $key = strval($key);//设置项目id前缀避免重复覆盖
+        $key = strval($key);
 
-        //使用redis 与cache 2选一 start
-        /*if (empty($db)) {
-            $db = Yii::$app->redis;
+        if(self::$cacheType===2){
+            // redis
+            if (empty($db)) {
+                $db = Yii::$app->redis;
+            }
+            return $db->del($key);
+        }else if(self::$cacheType===1){
+            // file
+            if (empty($db)) {
+                $db = Yii::$app->cache;
+            }
+            return $db->delete($key);
         }
-        return $db->del($key);*/
-        //使用redis 与cache 2选一 end
 
+    }
 
-        //使用文件cache start 与上注释redis 2选一 默认使用的文件
-        if (empty($db)) {
-            $db = Yii::$app->cache;
+    /**
+     * 获取自增后值，设置key自增如果小于3都将设置过期时间，尽可能防止参数脏数据
+     * @param $key
+     * @param $expireTime int 多少秒后过期
+     * @param object $db 存储数据主体(默认redis或cache),保存到不同存储区时使用
+     * @return bool
+     */
+    public static function getIncrValAndLt3SetEx($key, $expireTime, $db = null)
+    {
+        $expireTime = intval($expireTime);
+        $key = strval($key);
+
+        if(self::$cacheType===2){
+            // redis
+            if (empty($db)) {
+                $db = Yii::$app->redis;
+            }
+
+            $incrNum = $db->incr($key);
+            if(intval($incrNum)<3){
+                $db->expire($key, $expireTime);
+            }
+            return $incrNum;
+        }else if(self::$cacheType===1){
+            // 文件
+            if (empty($db)) {
+                $db = Yii::$app->cache;
+            }
+            // 文件模式没有自增只能采用
+            $cacheIncrNum = $db->get($key);
+            $cacheIncrNum = intval($cacheIncrNum);
+            if($cacheIncrNum>0){
+                $cacheIncrNum = $cacheIncrNum + 1;
+                $db->set($key, $cacheIncrNum, $expireTime);
+            }else{
+                $cacheIncrNum = 1;
+                $db->set($key, $cacheIncrNum, $expireTime);
+            }
+
+            return $cacheIncrNum;
         }
-        return $db->delete($key);
-        //使用文件cache end
     }
 }
