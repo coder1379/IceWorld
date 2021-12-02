2b85c4ac7d28cc8fbd792680062d15bc884b5d5b
coder1379 <majiegame@163.com>
2021-12-02 22:26:21 +0800

    utf8问题

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -7,7 +7,7 @@ return [
             'tablePrefix' => 'm_',
             'username' => 'root',
             'password' => 'password',
-            'charset' => 'utf8mb4',
+            'charset' => 'utf8mb4',// 注意不要覆盖为utf8 要不容易出现特殊字符数据库不支持
         ],
         'redis' => [
             'class' => 'yii\redis\Connection',
