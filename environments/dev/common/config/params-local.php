4a4563d02772b2292fd17e44a8cb87a483df393b
coder1379 <majiegame@163.com>
2021-12-07 16:53:47 +0800

    wx

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -11,4 +11,9 @@ return [
     'api_root_url' => 'http://api.localice.com/',//api 域名生产地址 local中覆盖测试
     'admin_root_url' => 'http://admin.localice.com/',//admin后台域名生产地址 local中覆盖测试
     'admin_site_show_name'=>'测试环境-后台管理系统',//显示名称-可在local里覆盖标明测试环境
+    'weixin_config' => [
+        // 微信参数配置
+        'appid' => '',
+        'secret'=>''
+    ],
 ];
